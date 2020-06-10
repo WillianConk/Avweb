623373ac78c9bdc60e4490a23b52ac38aa725e3d
Willian Conk <willmgrfu@gmail.com>
2020-06-10 18:43:54 -0300

    Projeto criado

---
+++ src/main/java/com/willianconk/avweb/AvwebApplication.java
@@ -0,0 +1,13 @@
+package com.willianconk.avweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvwebApplication.class, args);
+	}
+
+}
